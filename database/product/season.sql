e18ffbdb03d8f065c9958be4ca3ae2f62bbf7045
Joel PETRAZOLLER <joel.petrazoller@labom2iformation.fr>
2025-11-17 09:19:08 +0100

    mise à jour

--- database/product/season.sql
+++ database/product/season.sql
@@ -1,10 +1,14 @@
 CREATE TABLE IF NOT EXISTS seasons (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
+    name VARCHAR(100) NOT NULL UNIQUE,
+    start_day   TINYINT NOT NULL,
+    start_month TINYINT NOT NULL,
+    end_day     TINYINT NOT NULL,
+    end_month   TINYINT NOT NULL
 );
 
-INSERT INTO seasons (name) VALUES
-('Spring'),
-('Summer'),
-('Autumn'),
-('Winter');
+INSERT INTO seasons (name, start_day, start_month, end_day, end_month) VALUES
+('Spring', 20, 3, 20, 6),
+('Summer', 21, 6, 22, 9),
+('Autumn', 23, 9, 20, 12),
+('Winter', 21, 12, 19, 3);
